c7225fa0a16c1d50234ac0c4e116077cc008f8ec
poymanov <n.poymanov@gmail.com>
2022-05-16 20:25:18 +0300

    Добавлен Docker-Compose

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @mixin IdeHelperUser
+ */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -46,7 +46,9 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            $value = $request->user()?->id ?? $request->ip();
+
+            return Limit::perMinute(60)->by((string) $value);
         });
     }
 }
